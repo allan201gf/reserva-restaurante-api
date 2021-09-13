aa940d0d790faafa555f02dbdfb5dd801119f22a
allan201gf <allan201gf@gmail.com>
2021-09-13 10:20:40 -0300

    Retirado comentário de código antigo

--- src/main/java/br/com/allangf/reservarestauranteapi/domain/entity/Reserva.java
+++ src/main/java/br/com/allangf/reservarestauranteapi/domain/entity/Reserva.java
@@ -26,10 +26,6 @@ public class Reserva {
     @Column(name="dataReservaCriada")
     private LocalDate dataReservaCriada;
 
-//    @ManyToOne
-//    @JoinColumn(name = "periodoDaReserva")
-//    private PeriodoDaReserva periodoDaReserva;
-
     @Column(name = "dia_reservado")
     private LocalDate diaReservado;
 
